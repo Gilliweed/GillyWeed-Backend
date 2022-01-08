a08ccfb96ac401a50647c52f51f69a68d1084964
Mohityadav94161 <mohityadav94161@gmail.com>
2022-01-08 15:19:43 +0530

    start

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require('./routes/user');
-const productRoute = require('./routes/product');
-const authRoute = require('./routes/auth');
-const cartRoute = require('./routes/cart');
+const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
+const authRoute = require("./routes/auth");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const cors = require("cors");
 dotenv.config();
 
 mongoose
@@ -15,12 +17,18 @@ mongoose
     console.log(err);
   });
 app.use(express.json());
-app.use('/api/user' , userRoute);
-app.use('/api/product' , productRoute);
-app.use('/api/auth' , authRoute);
-app.use('/api/cart' ,cartRoute)
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use("/api/user", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/order", orderRoute);
 
-
-app.listen(2000, () => {
+app.listen(5000, () => {
   console.log("server is running on port ");
 });
